9c57b7e5a7566225d44a9c8b7197eb5cb98e2af5
Kristen Swaim <kswaimdev@gmail.com>
2023-06-20 05:11:47 -0400

    suring things up

--- TimedAssignment.Data/Entities/Hate.cs
+++ TimedAssignment.Data/Entities/Hate.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TimedAssignment.Data.Entities
 {
   public class Hate
     {
+        [Key]
         public int Id {get; set;}
-        public int PostId {get; set;}
         public Guid OwnerId {get; set;}
+        public int PostId {get; set;}
+
     } 
 }
